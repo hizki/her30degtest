7b0fc2047ee2d3e55dfe1cb2d92df49e27e258e0
Guy Hizkiau <hizki@Hizkis-Macbook.local>
2011-04-16 20:48:22 +0300

    new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Her30degtest::Application.config.secret_token = 'ec501be4350e94afb75fd7239b36daf321a5bdf4377796615b00ddad99efb099f684b5270728b2e5b7fc9638d0bf44846f5e86dfc17ce4adbc02da1f3cee9a66'
